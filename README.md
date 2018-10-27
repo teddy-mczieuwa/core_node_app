f42ef11f7da0801d7592fa8d4b12dfc4659a98f6
teddy <teddymczieuwa@gmail.com>
2018-10-27 22:17:31 +0100

    added a parsed request logger, and a url parser. Also added a README.md file

---
+++ README.md
@@ -0,0 +1,1 @@
+Learning some core node.js
--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const http = require('http')
+const url = require('url')
 
 const server = http.createServer((req,res) => {
+
+    const parsedUrl = url.parse(req.url, true)
+
+    const pathname = parsedUrl.pathname
+
+    const trimmedPath = pathname.replace(/^\/+|\/+$/g,'')
+
     res.end('Hello world')
+
+    console.log('Requested url path: ' + trimmedPath)
 })
 
 server.listen(8080, () => console.log('Server running on port 8080'))
 
+//replace(/^\/+|\/+$/g,'')
